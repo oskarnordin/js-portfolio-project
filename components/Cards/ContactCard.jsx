4d7b9b99f62406ab37b488d2c3404303b906e35f
oskarnordin <oskarnordin1@gmail.com>
2025-09-07 17:45:59 +0200

    Round LinkedIn logo
    Round LinkedIn logo

--- components/Cards/ContactCard.jsx
+++ components/Cards/ContactCard.jsx
@@ -190,13 +190,21 @@ const ContactCard = () => {
           </svg>
         </SocialIcon>
         <SocialIcon
+          src='/img/linkedin.png'
           href='https://www.linkedin.com/in/oskar-nordin-908129b6/'
           target='_blank'
           aria-label='LinkedIn'
         >
-          <svg viewBox='0 0 24 24'>
-            <path d='M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm13.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z' />
-          </svg>
+          <img
+            src='/img/linkedin.svg'
+            alt='LinkedIn icon'
+            style={{
+              width: 48,
+              height: 48,
+              padding: 4,
+              filter: 'invert(1) brightness(2)',
+            }}
+          />
         </SocialIcon>
       </ContactIconsWrapper>
     </ContactCardContainer>
