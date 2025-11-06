c0da2fdc7de7f189c23c4b8f11ef6b9f5ec6aa08
oskarnordin <oskarnordin1@gmail.com>
2025-11-06 19:10:38 +0100

    Lazy loading
    Lazy loading

--- App.jsx
+++ App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Overlay from './components/Sections/Overlay';
-import SkillsSection from './components/Sections/Techstack';
-import PrologueSection from './components/Sections/Prologue';
-import HeroSection from './components/Sections/HeroSection';
-import FeaturedProjects from './components/Sections/Projects';
-import MoodboardSection from './components/Sections/Moodboard';
-import ContactSection from './components/Sections/Contact';
+const SkillsSection = React.lazy(() => import('./components/Sections/Techstack'));
+const PrologueSection = React.lazy(() => import('./components/Sections/Prologue'));
+const HeroSection = React.lazy(() => import('./components/Sections/HeroSection'));
+const FeaturedProjects = React.lazy(() => import('./components/Sections/Projects'));
+const MoodboardSection = React.lazy(() => import('./components/Sections/Moodboard'));
+const ContactSection = React.lazy(() => import('./components/Sections/Contact'));
 import { ThemeProvider } from 'styled-components';
 import { theme } from '/components/theme';
 import './style.css';
@@ -13,23 +14,29 @@ import 'hamburgers/dist/hamburgers.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Sections/Footer';
 import ProgressBar from './components/ProgressBar';
-
+import Spinner from './components/Spinner';
 function App() {
+
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        {/* <Navbar /> */}
-        <HeroSection />
-        <Overlay />
-        <PrologueSection />
-        <FeaturedProjects />
-        <SkillsSection />
-        <MoodboardSection />
-        <ContactSection />
-      </ThemeProvider>
-      <main></main>
-    </>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+
+        <div id="bg-overlay" aria-hidden="true" style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(255, 255, 255, 0.40)', zIndex: -1 }} />
+        <Navbar />
+        <Suspense fallback={<Spinner size="lg" overlay={true} />}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/herosection" replace />} />
+            <Route path="/herosection" element={<HeroSection />} />
+            <Route path="/prologue" element={<PrologueSection />} />
             <Route path="/showroom" element={<FeaturedProjects />} />
+            <Route path="/techstack" element={<SkillsSection />} />
+            <Route path="/moodboard" element={<MoodboardSection />} />
+            <Route path="/contact" element={<ContactSection />} />
+          </Routes>
+        </Suspense>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
